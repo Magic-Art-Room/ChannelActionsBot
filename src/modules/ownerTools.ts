ff37648bca0046d51db205f6cdc124c6458bcb54
Magic-Art-Room <99250685+Magic-Art-Room@users.noreply.github.com>
2023-07-06 10:04:28 +0530

    Update ownerTools.ts

--- src/modules/ownerTools.ts
+++ src/modules/ownerTools.ts
@@ -36,10 +36,9 @@ composer
 <b>Total users</b>: ${await countUsers()}
 <b>Chats with modified settings</b>: ${(await getAllSettings()).length}
 <b>Total Users Seen (Approved/Disapproved)</b>: ${helperClass.TOTAL_USERS_SEEN}
-<b>Uptime</b>: ${uptime}
+<b>Uptime</b>: ${uptime}',
 
-<b><a href="https://github.com/xditya/ChannelActionsBot">Repository</a> | <a href="https://t.me/BotzHub">Channel</a> | <a href="https://t.me/BotzHubChat">Support</a></b>`,
-      { parse_mode: "HTML", disable_web_page_preview: true },
+  { parse_mode: "HTML", disable_web_page_preview: true },
     );
   });
 
